3f7416547950c19ae08accc99255d4866957ea6c
Ângelo Gabriel <119939464+angelocsdev@users.noreply.github.com>
2025-02-07 15:48:11 +0000

    variaveis

---
+++ 2-variaveis/n1_nc4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // Exemplos de variáveis
+
+    int idade = 22; // armazena valores inteiros
+    int quantidade = 400;
+    float altura = 1.75; // casas decimais
+    double peso = 77.56; // casas decimais com valores maiores
+    char letra = 'a'; // armazena caracteres
+    char nome[20] = "Ângelo"; // armazena strings quando cria arrays
+
+}
